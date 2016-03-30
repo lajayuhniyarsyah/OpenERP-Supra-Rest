017a24e8b5dcc681053d731c155db0515d358ee2
Mohammadrezafah <mrfahlepi@gmail.com>
2016-03-30 03:42:47 -0700

    url root

--- supra_api/supra_api/wsgi.py
+++ supra_api/supra_api/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Django_api.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "supra_api_Rest.settings")
 
 application = get_wsgi_application()
